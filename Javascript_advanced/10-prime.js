b8d2a757f70a9b3828e5e7fcd6aa0d0bef554c2f
Tsimmuaj <tyang4204@gmail.com>
2025-04-03 00:08:54 -0500

    really good computer

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,24 @@
+const countPrimeNumbers = () => {
+  let prime = 0;
+
+  const check = (num) => {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+  for (let i = 2; i <= 100; i++) {
+    if (check(i)) {
+      prime++;
+    }
+  }
+  return prime;
+}
+
+const start = performance.now();
+for (let i = 0; i < 100; i++) {
+  countPrimeNumbers();
+}
+const end = performance.now();
+console.log(`Execution time of calculating prime numbers 100 times was ${(end - start)} milliseconds.`)
